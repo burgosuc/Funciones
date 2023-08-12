237bcd8a15779e3e2319cca96f7183950f7d0035
burgosuc <jlb1983@gmail.com>
2023-08-12 00:19:07 -0400

    first commit

---
+++ Requerimiento 1/assets/js/1_funcion.js
@@ -0,0 +1,16 @@
+/*Transforma esta declaración de función a una función de expresión.
+
+function example(a, b, c){
+return a+b+c
+}*/
+
+//Transformación a expresion de funcion
+const example = function (a, b, c) {
+  return a + b + c;
+}
+
+//Para llamar la función se utiliza
+resultado = example(3,4,3);
+
+//Si se quiere mostrar o imprimir en la consola
+console.log(example); //Mostrara como resultado 10.
---
+++ Requerimiento 2/assets/js/2_arrow.js
@@ -0,0 +1,8 @@
+/*Transforma esta declaración de función a una función de expresión.
+
+let suma = function(a, b){
+return a + b
+}*/
+
+//Transformación a arrow function
+const suma = (a, b) => a + b;
